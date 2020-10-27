08be8ab0216ebe067c1efbdf6d0a30a4463af1e1
jay.park <jay.park@kakaocorp.com>
2020-10-27 19:15:33 +0900

    initial commit

---
+++ src/main/kotlin/com/kakao/jaypark/grapesticker/GrapeStickerApplication.kt
@@ -0,0 +1,11 @@
+package com.kakao.jaypark.grapesticker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrapeStickerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrapeStickerApplication>(*args)
+}
---
+++ src/test/kotlin/com/kakao/jaypark/grapesticker/GrapeStickerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kakao.jaypark.grapesticker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrapeStickerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
