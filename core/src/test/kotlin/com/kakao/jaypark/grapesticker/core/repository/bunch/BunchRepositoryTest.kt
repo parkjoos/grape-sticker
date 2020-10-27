de2eaed316f830fa8ecf9f3717dbffb1feba0630
jay.park <jay.park@kakaocorp.com>
2020-10-27 20:00:59 +0900

    start repository test

---
+++ core/src/test/kotlin/com/kakao/jaypark/grapesticker/core/repository/bunch/BunchRepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.kakao.jaypark.grapesticker.core.repository.bunch
+
+import com.kakao.jaypark.grapesticker.core.CoreApplication
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(classes = [CoreApplication::class])
+class BunchRepositoryTest {
+
+    @Test
+    fun test(){
+
+    }
+}
