9472b0c689a47fd4d40b9f1b3e14dace8fbc4d2b
jay.park <jay.park@kakaocorp.com>
2020-11-01 13:14:39 +0900

    grape-sticker-6 Setup test or local dynamodb environment https://github.com/parkjoos/grape-sticker/issues/6 - fix timezone

--- core/src/main/kotlin/com/kakao/jaypark/grapesticker/core/domain/converter/LocalDateTimeConverter.kt
+++ core/src/main/kotlin/com/kakao/jaypark/grapesticker/core/domain/converter/LocalDateTimeConverter.kt
@@ -2,13 +2,13 @@ package com.kakao.jaypark.grapesticker.core.domain.converter
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter
 import java.time.LocalDateTime
-import java.time.ZoneOffset
+import java.time.ZoneId
 import java.util.*
 
 
 class LocalDateTimeConverter : DynamoDBTypeConverter<Date, LocalDateTime> {
     override fun convert(source: LocalDateTime): Date {
-        return Date.from(source.toInstant(ZoneOffset.UTC))
+        return Date.from(source.atZone(ZoneId.systemDefault()).toInstant())
     }
 
     override fun unconvert(source: Date): LocalDateTime {
