ad1736268d5f1b32b7dce28cd3ca0937b8fbb797
jay.park <jay.park@kakaocorp.com>
2020-11-01 15:52:39 +0900

    [grape-sticker-3] Controllers - bunch https://github.com/parkjoos/grape-sticker/issues/3 - sub module clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.4.RELEASE"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+	base
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
 }
 
-group = "com.kakao.jaypark"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
-
-repositories {
-	mavenCentral()
+allprojects {
+	group = "com.kakao.jaypark"
+	version = "0.0.1-SNAPSHOT"
+	repositories {
+		mavenCentral()
+		maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+		maven(url = "https://s3.ap-northeast-1.amazonaws.com/dynamodb-local-tokyo/release") // for DynamoDBLocal Lib
+	}
 }
 
 dependencies {
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	subprojects.forEach {
+		archives(it)
 	}
-	testImplementation("org.junit.jupiter:junit-jupiter-api")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-	testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.withType<Test> {
